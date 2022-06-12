4468f20599c2dd24f3131dbee13346e557936bca
ramos2022y <ramos2022y@gmail.com>
2022-06-12 12:43:53 +0800

    Add index.html

--- .maintain/deploy.sh
+++ .maintain/deploy.sh
@@ -1,32 +1,35 @@
-git pull
+# git pull
 
-mkdir -p _deploy
-rm -rf _deploy/*
+# mkdir -p _deploy
+# rm -rf _deploy/*
 
-#en
-git checkout lang-en
-bundle exec jekyll build
-cp -rf _site _deploy/en
+# #en
+# git checkout lang-en
+# bundle exec jekyll build
+# cp -rf _site _deploy/en
 
-# zh-cn
-git checkout main
-bundle exec jekyll build
-cp -rf _site _deploy/zh-cn
+# # zh-cn
+# git checkout main
+# bundle exec jekyll build
+# cp -rf _site _deploy/zh-cn
 
-# zh-tw
-git checkout lang-zh-tw
-bundle exec jekyll build
-cp -rf _site _deploy/zh-tw
+# # zh-tw
+# git checkout lang-zh-tw
+# bundle exec jekyll build
+# cp -rf _site _deploy/zh-tw
 
-# kr
-git checkout lang-kr
-bundle exec jekyll build
-cp -rf _site _deploy/kr
+# # kr
+# git checkout lang-kr
+# bundle exec jekyll build
+# cp -rf _site _deploy/kr
 
-# jp
-git checkout lang-jp
-bundle exec jekyll build
-cp -rf _site _deploy/jp
+# # jp
+# git checkout lang-jp
+# bundle exec jekyll build
+# cp -rf _site _deploy/jp
+
+#
+cp .maintain/index.html _deploy/
 
 # Checkout to main
 git checkout main
