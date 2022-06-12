02a6fa0df4de2607514aaab71c845748858f6048
ramos2022y <ramos2022y@gmail.com>
2022-06-12 16:03:54 +0800

    Fix deploy.sh

--- .maintain/deploy.sh
+++ .maintain/deploy.sh
@@ -1,30 +1,33 @@
-git pull
-
 mkdir -p _deploy
 rm -rf _deploy/*
 
 #en
 git checkout lang-en
+git pull
 bundle exec jekyll build
 cp -rf _site _deploy/en
 
 # zh-cn
 git checkout main
+git pull
 bundle exec jekyll build
 cp -rf _site _deploy/zh-cn
 
 # zh-tw
 git checkout lang-zh-tw
+git pull
 bundle exec jekyll build
 cp -rf _site _deploy/zh-tw
 
 # kr
 git checkout lang-kr
+git pull
 bundle exec jekyll build
 cp -rf _site _deploy/kr
 
 # jp
 git checkout lang-jp
+git pull
 bundle exec jekyll build
 cp -rf _site _deploy/jp
 
